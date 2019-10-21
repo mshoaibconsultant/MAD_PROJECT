f70463ce6a2b53b63f032eae992a39cf3e3c0fa3
mshoaib.consultant <shoaib7878@hotmail.com>
2019-10-22 00:14:39 +0500

    Assignment_03
    Submit Assignment_03, Dear Sir please accept, Muhammad Shoaib.

---
+++ Assignment_02/app/src/main/java/com/example/myapplication/LayoutCodeLab.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LayoutCodeLab extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_code_lab);
+    }
+}
