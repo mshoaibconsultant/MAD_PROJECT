0a3522b8962435e12880817658b9ffad432613b2
mshoaib.consultant <shoaib7878@hotmail.com>
2019-10-27 00:59:11 +0500

    Assignment_04
    Assignment_04, Added Validation on Signup and Login Screen,  Call 1 Activity to Another.

---
+++ Assignment_02/app/src/main/java/com/example/myapplication/a_4_login.java
@@ -0,0 +1,44 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class a_4_login extends AppCompatActivity {
+    TextView tv;
+    EditText username;
+    EditText password;
+    EditText email;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a_4_login);
+        tv = findViewById(R.id.textView);
+        username = findViewById(R.id.editText);
+        password = findViewById(R.id.editText1);
+        email = findViewById(R.id.editText2);
+    }
+
+    public void EmailValidate1(View v){
+        String str = email.getText().toString();
+        String str1 = password.getText().toString();
+        boolean isError= false;
+        if (!str.contains("@")){
+            email.setError("@ is missing");
+            isError = true;}
+        if (str1.length() < 5 || str1.length() > 8){
+            password.setError("invalid pass");
+            isError = true;}
+
+        if(!isError){
+            Intent intent = new Intent(this,BeautyandBeast.class);
+            startActivity(intent);
+        }
+    }
+}
---
+++ Assignment_02/app/src/main/java/com/example/myapplication/a_4_signup.java
@@ -0,0 +1,47 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class a_4_signup extends AppCompatActivity {
+    TextView tv;
+    EditText username;
+    EditText password;
+    EditText email;
+    CheckBox cb;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a_4_signup);
+        tv = findViewById(R.id.textView);
+        username = findViewById(R.id.editText);
+        password = findViewById(R.id.editText1);
+        email = findViewById(R.id.editText2);
+        cb = findViewById(R.id.checkbox);
+    }
+
+    public void EmailValidate(View v){
+        String str = email.getText().toString();
+        String str1 = password.getText().toString();
+        boolean isError= false;
+        if (!str.contains("@")){
+            email.setError("@ is missing");
+            isError = true;}
+        if (str1.length() < 5 || str1.length() > 8){
+            password.setError("invalid pass");
+            isError = true;}
+
+        if(!isError){
+            Intent intent = new Intent(this,a_4_login.class);
+            startActivity(intent);
+        }
+    }
+}
